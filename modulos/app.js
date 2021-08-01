283885c72ce93d1f112e509fb01346d567157119
diegoaldemar <diegoaldemar95@gmail.com>
2021-07-31 23:24:30 -0500

    practica modulos path y os

--- modulos/app.js
+++ modulos/app.js
@@ -1,2 +1,20 @@
-import './datos';
+const datos = require('./datos');
+const path = require('path');
+const os = require('os')
+
+
+datos.log('holaa');
+
+console.log(__dirname);
+console.log(__filename);
+
+
+console.log(path.parse(__dirname))//obtenemos el path convertido en un objeto
+console.log(path.parse(__filename).ext) //obtenemos file path en un obejto, podemos obtener la ext, la base, name del mismo
+
+const cpus = os.cpus()
+console.log(cpus)
+
+const memoriaLibre = os.freemem()
+console.log(`memoria libre: ${memoriaLibre.toExponential()}`)
 
